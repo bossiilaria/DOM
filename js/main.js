a68bc6fc8ead8389cad51b2b7af25e59e4409c18
Ilaria Bossi <bossiilaria@users.noreply.github.com>
2015-11-25 04:12:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var avatarEls = document.getElementsByTagName("img");
 
 //change the size of the 3 avatars
-for(var i = 0; i < 3; i++) {
+for(var i = 0; i < avatarEls.length; i++) {
 	avatarEls[i].height= 150;
 }
