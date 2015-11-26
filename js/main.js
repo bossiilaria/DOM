e3024ce29f9d70a9a20132c5d7d048ea92638ac5
Ilaria Bossi <bossiilaria@users.noreply.github.com>
2015-11-25 23:57:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,14 @@ var avatarEls = document.getElementsByTagName("img");
 
 //change the size of the 3 avatars
 for(var i = 0; i < avatarEls.length; i++) {
-	avatarEls[i].height= 150;
+	avatarEls[i].height= 200;
 }
+
+var namePink = document.getElementById("pink");
+namePink.innerHTML="Winston";
+
+var nameGreen = document.getElementById("green");
+nameGreen.innerHTML="Watson";
+
+var nameOrange = document.getElementById("orange");
+nameOrange.innerHTML="Watsup";
