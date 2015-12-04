ed4502c52a81d0814d7e2f9b2c1b0d1f863cde26
Ilaria Bossi <bossiilaria@users.noreply.github.com>
2015-12-03 22:53:05 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
+var myStart;
+
+function myFunction() {
+    myVar = setInterval(alertFunc, 3000);
+}
+
+function alertFunc() {
+    alert("Hello!");
+}
+
+//Use clearInterval to stop
+
+function stopMyFunction() {
+	clearInterval(myVar);
+}
+//Practice body.innerHTML: Make the whole content disappear
+/*
+	document.body.innerHTML = "";
+*/
+
+//Change the size of the 3 avatars
 var avatarEls = document.getElementsByTagName("img");
 
-//change the size of the 3 avatars
 for(var i = 0; i < avatarEls.length; i++) {
-	avatarEls[i].height= 200;
+	avatarEls[i].height = 160;
 }
+
 
 var namePink = document.getElementById("pink");
 namePink.innerHTML="Winston";
@@ -13,3 +34,9 @@ nameGreen.innerHTML="Watson";
 
 var nameOrange = document.getElementById("orange");
 nameOrange.innerHTML="Watsup";
+
+
+var headingEl = document.querySelector("#heading");
+headingEl.style.color = "green";
+headingEl.style.backgroundColor = "pink";
+headingEl.style.textAlign = "center";
